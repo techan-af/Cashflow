acf29d3f90670f4c2b6dac4b9bc8710585ae8bea
Chetan Singh <chetan95497@gmail.com>
2023-06-24 18:07:43 +0530

    Added tailwind css

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,4 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
@@ -8,7 +6,9 @@ function App() {
 
   return (
     <>
-     
+      <h1 className="text-3xl underline">
+        Hello world!
+      </h1>
     </>
   )
 }
